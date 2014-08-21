d8b8e2fd6ea4d93d01e37add33cb41fd297fdd75
Mackenzie High <mhigh@gmu.edu>
2014-08-21 12:45:26 -0400

    removing files

--- build-latest.sh
+++
@@ -1,6 +0,0 @@
-
-rm -r "/media/disk/bin/autumn-latest/"
-
-cp -r "/media/disk/Code/NetBeansProjects/autumn/dist" "/media/disk/bin/autumn-latest/"
-
-mv "/media/disk/bin/autumn-latest/AutumnCompiler.jar" "/media/disk/bin/autumn-latest/autumn.jar"
