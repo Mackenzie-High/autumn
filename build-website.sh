548cae71fc27aa1b356a20df2db62adecd69596e
Mackenzie High <mhigh@gmu.edu>
2015-04-15 16:49:24 -0400

    update website

--- build-website.sh
+++ build-website.sh
@@ -2,4 +2,11 @@
 
 # Replace the old javadoc with the new javadoc.
 rm -rf "javadoc/"
-mv "/media/disk/Code/NetBeansProjects/autumn/dist/javadoc" "javadoc"
+cp -r "/media/disk/Code/NetBeansProjects/autumn/dist/javadoc" "javadoc"
+
+
+# Commit the changes and push the updates to the server.
+git add .
+git commit -am "update website"
+git push "origin" "gh-pages"
+
